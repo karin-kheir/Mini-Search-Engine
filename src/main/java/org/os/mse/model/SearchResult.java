e0193cbb2e2950bc306c2f87fb541ee7f5e52202
Karin Kheir <karinkheir@gmail.com>
2026-01-13 21:27:27 +0200

    SearchResult code

--- src/main/java/org/os/mse/model/SearchResult.java
+++ src/main/java/org/os/mse/model/SearchResult.java
@@ -1,4 +1,29 @@
 package org.os.mse.model;
 
 public class SearchResult {
+    private String documentName;
+    private int score;
+
+    public SearchResult(){}
+
+    public SearchResult(String documentName, int score){
+        this.documentName = documentName;
+        this.score = score;
+    }
+
+    public String getDocumentName() {
+        return documentName;
+    }
+
+    public void setDocumentName(String documentName) {
+        this.documentName = documentName;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
