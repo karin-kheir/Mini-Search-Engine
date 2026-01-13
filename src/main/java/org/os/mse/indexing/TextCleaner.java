a762e4aa14df8d9e146fff880586dc8662005388
Karin Kheir <karinkheir@gmail.com>
2026-01-13 18:28:53 +0200

    Add TextCleaner logic

--- src/main/java/org/os/mse/indexing/TextCleaner.java
+++ src/main/java/org/os/mse/indexing/TextCleaner.java
@@ -1,4 +1,11 @@
 package org.os.mse.indexing;
 
 public class TextCleaner {
+
+    public static String clean(String content){
+        content = content.toLowerCase();
+        content = content.replaceAll("[^a-zA-Z0-9 ]", "");
+        content = content.replaceAll("\\s+", " ").trim();
+        return content;
+    }
 }
