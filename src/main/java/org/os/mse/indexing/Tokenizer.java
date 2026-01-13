9b4e6b9676d67cdeee43ce16fcbfb013105e64c3
Karin Kheir <karinkheir@gmail.com>
2026-01-13 19:18:27 +0200

    Tokenizer logic

--- src/main/java/org/os/mse/indexing/Tokenizer.java
+++ src/main/java/org/os/mse/indexing/Tokenizer.java
@@ -1,4 +1,14 @@
 package org.os.mse.indexing;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Tokenizer {
+    public static List<String> tokenize(String content) {
+
+        String[] newContent = content.split("\\s+");
+        return Arrays.stream(newContent)
+                .filter(token -> !token.isBlank())
+                .toList();
+    }
 }
