9b496ebdabadca826c177e6f8a1dc4e7332a8856
Karin Kheir <karinkheir@gmail.com>
2026-01-13 21:50:28 +0200

    Add files to test

--- src/main/java/org/os/mse/search/Ranker.java
+++ src/main/java/org/os/mse/search/Ranker.java
@@ -1,4 +1,12 @@
 package org.os.mse.search;
 
+import org.os.mse.model.SearchResult;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Ranker {
+    public static void rank(List<SearchResult> results){
+        Collections.sort(results, Comparator.comparingInt(SearchResult::getScore).reversed());
+    }
 }
