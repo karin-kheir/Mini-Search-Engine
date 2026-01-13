e38cc3f85e4003c65ce48e318ec426a7c501c03b
Karin Kheir <karinkheir@gmail.com>
2026-01-13 18:25:51 +0200

    Remove IntelliJ files from tracking and update .gitignore

--- src/main/java/org/os/mse/model/Document.java
+++ src/main/java/org/os/mse/model/Document.java
@@ -1,4 +1,35 @@
 package org.os.mse.model;
 
 public class Document {
+    String path;
+    String content;
+
+    public Document(String path, String content){
+        this.path = path;
+        this.content = content;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Document{" +
+                "path='" + path + '\'' +
+                ", contentLength=" + content.length() +
+                '}';
+    }
 }
--- src/main/java/org/os/mse/storage/FileLoader.java
+++ src/main/java/org/os/mse/storage/FileLoader.java
@@ -1,4 +1,30 @@
 package org.os.mse.storage;
 
+import org.os.mse.model.Document;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileLoader {
+    public static List<Document> loadDocuments(String folderPath) throws IOException {
+        List<Document> documents = new ArrayList<>();
+        File folder = new File(folderPath);
+        if (!folder.isDirectory() || !folder.exists()){
+            throw new IOException("Invalid folder path" + folderPath);
+        }
+
+        File[] files = folder.listFiles((dir, name) -> name.endsWith(".txt"));
+
+        if (files == null){
+            throw new IOException("There is no files");
+        }
+        for(File file : files){
+            String content = Files.readString(file.toPath());
+            documents.add(new Document(file.getPath(), content));
+        }
+
+        return documents;
+    }
 }
